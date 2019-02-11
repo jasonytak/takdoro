2d6b62625ae44715b9e18d739e501680b3644d93
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-02-11 15:32:42 -0800

    Set up to serve static files

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if (process.env.NODE === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('root route');
 });
 
-app.listen(PORT, (req, res) => {
-  console.log(`Server listeing on ${PORT}`);
-})
+app.listen(PORT);
