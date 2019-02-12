cfd67290d58f41ad8934d105feb7180cd2a69924
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-02-12 14:55:49 -0800

    Adjusted file structure for code readability

---
+++ client/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  state = { users: [] };
+
+  render() {
+    return <div>HELLO WORLD</div>;
+  }
+}
+
+export default App;
