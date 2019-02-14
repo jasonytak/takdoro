5e7ea3599e213693c1ef87394a8e0a72f88d1e33
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-02-14 13:47:44 -0800

    Added LoginPage/HomePage routes

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginPage from '../LoginPage/LoginPage';
+import HomePage from '../HomePage/Homepage';
 import axios from 'axios';
 
 class App extends Component {
@@ -19,9 +21,12 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <LoginPage onSubmit={this.onSubmit} />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={LoginPage} />
+          <Route path="/home" component={HomePage} />
+        </div>
+      </Router>
     );
   }
 }
