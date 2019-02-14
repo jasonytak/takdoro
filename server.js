a7e78afbc45da7386f93687d81e95d6997bb2a5d
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-02-14 14:19:28 -0800

    Added router handler for specific time

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get('/time', (req, res) => {
+  res.locals.seconds = new Date().getMinutes() + new Date().getSeconds();
+  res.send(res.locals);
+});
+
 app.post('/user', userController.saveAndFind);
 
 // Heroku will inject product env variables
