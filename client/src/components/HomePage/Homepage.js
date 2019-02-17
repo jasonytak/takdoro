bfefaf4ffd7c95b1c48d948c33514f7acb71c188
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-02-16 16:36:46 -0800

    Added classname

--- client/src/components/HomePage/Homepage.js
+++ client/src/components/HomePage/Homepage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import TimerControl from '../TimerControl/TimerControl';
 
 const HomePage = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="home-page">
+      <TimerControl />
+    </div>
+  );
 };
 
 export default HomePage;
