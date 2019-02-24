10975bc22af13994b27447a3f33ed5b35ce87207
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-02-23 16:15:04 -0800

    Added new methods for add and finding users

--- models/userController.js
+++ models/userController.js
@@ -2,12 +2,28 @@ const User = require('./User');
 
 const userController = {};
 
-userController.saveAndFind = async (req, res) => {
-  await User.create({ user: req.body.user });
-  await User.find({}, (err, user) => {
+userController.createUser = (req, res) => {
+  User.create({ user: req.body.user }, (err, user) => {
     if (err) throw err;
     res.send(user);
   });
 };
+
+userController.findCurrentUsers = (req, res) => {
+  User.find({}, (err, user) => {
+    if (err) throw err;
+    res.send(user);
+  });
+};
+
+// userController.saveAndFind = async (req, res) => {
+//   await User.create({ user: req.body.user }, (err, user) => {
+//     console.log(user);
+//   });
+//   await User.find({}, (err, user) => {
+//     if (err) throw err;
+//     res.send(user);
+//   });
+// };
 
 module.exports = userController;
