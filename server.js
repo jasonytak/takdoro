847046bd08ee09c0db7fc4440bf21d7601f079e2
Jason Tak <Tak@Jasons-MacBook-Pro.local>
2019-03-07 17:03:52 -0800

    User refresh problem

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.post('/user', userController.createAndFind);
 if (process.env.NODE === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('/*', (req, res) => {
+    console.log('HELLO');
     res.sendFile(path.join(__dirname, 'build/index.html'));
   });
 }
